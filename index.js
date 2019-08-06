fdba2b3f0b627bef9b0c98603f7a655560327f67
Cliff Karlsson <ntn15cko@student.hig.se>
2019-08-06 08:34:40 +0200

    added lint

--- index.js
+++ index.js
@@ -115,10 +115,11 @@ const errorHandler = (error, request, response, next) => {
         return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
-    } else {
-        console.log(`Error name: ${error.name}`)
-        return response.status(400).json({ error: error.message })
     }
+    /*  } else {
+         console.log(`Error name: ${error.name}`)
+         return response.status(400).json({ error: error.message })
+     } */
     next(error)
 }
 
